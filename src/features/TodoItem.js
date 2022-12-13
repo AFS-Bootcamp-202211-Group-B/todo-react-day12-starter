66151a262bed0e79afc479ea8046c1aad5ac8a85
tokaho <tokaho2000@gmail.com>
2022-12-13 17:04:22 +0800

    [Feat] add css

--- src/features/TodoItem.js
+++ src/features/TodoItem.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { useDispatch } from 'react-redux'
 import { toggleTodo } from "./todoSlice";
 
 export default function TodoItem(props) {
   const dispatch = useDispatch()
   return <div 
-          onClick={() => {
-            dispatch(toggleTodo(props.todo.id))
-          }}
-          style={{"textDecorationLine":props.todo.done?"line-through":"null"}}
+            onClick={() => {
+              dispatch(toggleTodo(props.todo.id))
+            }}
+            className={props.todo.done?"done":"notDone"}
           >
-    {props.todo.text}
-  </div>;
+            {props.todo.text}
+          </div>
 }
