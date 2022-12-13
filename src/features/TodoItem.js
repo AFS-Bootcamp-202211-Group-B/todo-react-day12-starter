f844c5d6d321465db722fcb2b0000ab47dbb65e6
WONGCH9 <wongch9@oocl.com>
2022-12-14 00:47:50 +0800

    feat: add toggle item & delete item

--- src/features/TodoItem.js
+++ src/features/TodoItem.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleItem,deleteItem } from './itemSlice';
+import { useState } from 'react';
+import './Todoitem.css';
 
 export default function TodoItem(props) {
-  return <div>{props.todo}</div>;
+
+  const dispatch = useDispatch();
+  const {todo} = props;
+
+  const [btnState, setBtnState] = useState("");
+
+  const toggleItemClick =  () =>{
+
+    if(btnState === ""){
+      setBtnState("toggle");
+    }
+    else{
+      setBtnState("");
+    }
+
+    //console.log(todo);
+    dispatch(toggleItem(todo));
+
+
+  };
+
+  const crossItemClick =  () =>{
+
+    dispatch(deleteItem(todo));
+
+  };
+
+  return ( 
+      <div className={btnState} onClick={ toggleItemClick } id="css" > 
+          {todo.text} 
+          <span onClick={crossItemClick} className="cross">
+          </span>
+      </div>
+  );
 }
