22edfa325fef63453cff022a2f681e81be5cbf33
leungpo <polly.leung@oocl.com>
2022-12-13 20:37:19 +0800

    feat: improve UI for close button

--- src/features/TodoItem.js
+++ src/features/TodoItem.js
@@ -5,10 +5,9 @@ import {deleteToDo} from "./todoListSlice";
 import "./TodoItem.css"
 export default function TodoItem(props) {
 const dispatch = useDispatch();
-const classes = ` cursorchange ${props.todo.done?"line":""}`;
-  return <div onClick={() => {dispatch(changeDone(props.todo.id))}} 
-  className={classes}>
-    {props.todo.text}<button className="close-button" aria-label="Close alert" type="button"><span className="clickcursor" aria-hidden="true" onClick={() => dispatch(deleteToDo(props.todo.id))}>&times;</span></button>
-
+const classes = ` text ${props.todo.done?"line":""}`;
+  return <div className="box">
+    <span onClick={() => {dispatch(changeDone(props.todo.id))}} 
+  className={classes}>{props.todo.text}</span><span className="close-button" onClick={() => dispatch(deleteToDo(props.todo.id))}>&times;</span>
   </div>;
 }
